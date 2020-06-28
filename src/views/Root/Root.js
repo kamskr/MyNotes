517603774b4ad1c1b5f47e0e3c95224cc40b612b
Kamil <sikorakamil26>
2020-06-28 20:59:00 +0200

    Added button styled component

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Button from 'components/Button/Button';
 
-const Root = () => <h1>hello</h1>;
+const Root = () => (
+  <div>
+    <h1>hello</h1>
+    <Button width="500px">CLOSE / SAVE</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
 
 export default Root;
