597a82c7b728890c826d16f2afa501b2b5f95cc9
Kamil <sikorakamil26>
2020-06-30 22:48:01 +0200

    added global theme

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -3,12 +3,12 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
-  width: ${({ width }) => width || '220px'};
+  background-color: ${({ theme }) => theme.primary};
+  width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
-  font-weight: 500;
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
 
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import Button from 'components/Button/Button';
+import React, { Fragment } from 'react';
+import { ThemeProvider } from 'styled-components';
+import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>hello</h1>
-    <Button>CLOSE / SAVE</Button>
-    <Button secondary>Remove</Button>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <h1>hello</h1>
+        <Button>CLOSE / SAVE</Button>
+        <Button secondary>Remove</Button>
+      </Fragment>
+    </ThemeProvider>
   </div>
 );
 
